4f14072fea3f5e302424683bf1f002644eb4feff
miquelm51 <119348739+miquelm51@users.noreply.github.com>
2022-12-02 10:50:47 +0100

    l3

--- modulo1/l3/l3s.js
+++ modulo1/l3/l3s.js
@@ -1,5 +1,5 @@
-const pokemons [pokemon={tipo:1 , nombre:"pepe"},dinosaurio={tipo:2,nombre:dino}];
+let pokemos= [pokemon={tipo:1 , nombre:"pepe"},dinosaurio={tipo:2,nombre:"dino"}];
 if (pokemon.tipo==1)
-   console.log ("es el pokemon pepe);
+   console.log ("es el pokemon pepe");
 if  (dinosaurio.tipo==2)
      console.log("es el dinosaurio dino");
